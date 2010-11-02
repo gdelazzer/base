40ddf396f10182173a61dd8c529d39db5c6c5a2f
Guillaume De Lazzer <guillaume@ejahworld.net>
2010-11-02 14:06:42 +0800

    Return context_instance in main view

--- base/main/views.py
+++ base/main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 def index(request):
-    return render_to_response("base.html")
+    return render_to_response("base.html",
+                              context_instance=RequestContext(request))
